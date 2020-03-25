80b98914d9490e538c876b4c0c935c7ca2e54b33
Jean-Marc Flamand <7jtf@videotron.ca>
2020-03-25 15:17:50 -0400

    Tab Navigation project
    Add  basic example of navigation between views using tab control.

---
+++ CMTabNavigation/README.md
@@ -0,0 +1,1 @@
+# PackageReference Caliburn.Micro" Version="4.0.105-alpha"
---
+++ CMTabNavigation/ViewModels/ShellViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CMTabNav.ViewModels
+{
+    public class ShellViewModel : Conductor<object>.Collection.OneActive
+    {
+        public ShellViewModel()
+        {
+            
+        }
+
+        public void PartsList()
+        {
+            //ActivateItemAsync(IoC.Get<PartsListViewModel>(), new CancellationToken()
+            //{ DisplayName = "Part List" });
+
+            ActivateItemAsync(IoC.Get<PartsListViewModel>(), new CancellationToken());
+             DisplayName = "Part List";
+
+        }
+        public void AssembliesList() //AssembliesList
+        {
+            ActivateItemAsync(IoC.Get<AssembliesListViewModel>(), new CancellationToken());
+            DisplayName = "Assembly List";
+        }
+        public void Demo()
+        {
+            ActivateItemAsync(IoC.Get<DemoViewModel>(), new CancellationToken());
+            DisplayName = "Demo View";
+        }
+
+
+
+    }
+}
